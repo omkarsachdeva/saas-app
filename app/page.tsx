cea5543be07090a0cd679f4e0a14e1c6425f7d46
omkarsachdeva <omkarsachdeva3832stella@gmail.com>
2025-06-30 22:12:30 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>
-        Let's get started!
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+        id="123"
+        name="Neura the brainy explorer"
+        topic="Neural network of the brain"
+        subject="Science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="456"
+        name="Countsy the number wizard"
+        topic="derivatives and integrals"
+        subject="Maths"
+        duration={30}
+        color="#e5d0ff"
+        />
+        <CompanionCard
+        id="789"
+        name="Verba the vocabulary builder"
+        topic="language"
+        subject="English literature"
+        duration={30}
+        color="#BDE7FF"
+        />
+       
+      </section>
+
+      <section className='home-section'>
+
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanionsList = () => {
+  return (
+    <div>
+      Companions list
+    </div>
+  )
+}
+
+export default CompanionsList
